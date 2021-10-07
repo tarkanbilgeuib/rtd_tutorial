dc8bfdbdb3c4ae7085167005c04b299363d0cdc1
Tarkan Bilge <tbi045@gfi3103039.klientdrift.uib.no>
2021-10-07 11:48:15 +0200

    Added a module called my_module.py.

---
+++ my_module.py
@@ -0,0 +1,12 @@
+""" Module to test out Sphinx automatic documentation """
+
+def my_func(word):
+    """ An overdocumented print function
+
+    Parameters
+    ----------
+    word : str
+        A word to print.
+    """
+    print(word)
+    
